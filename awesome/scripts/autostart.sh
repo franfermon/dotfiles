6736fdbcb0598088967e5d7364b46e0c80206bd1
franfermon <franmontes91@gmail.com>
2023-10-09 22:39:51 +0200

    First commit

---
+++ awesome/scripts/autostart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep -f $1 ;
+  then
+    $@&
+  fi
+}
+
+# Policy kit (needed for GUI apps to ask for password)
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# Start compositor
+picom &
+# Nitrogen wallpaper
+run nitrogen --restore &
